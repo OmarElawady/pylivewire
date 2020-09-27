772bf1f52963be05ea20406fe4387a7359183ade
Omar Elawady <omarelawady1998@gmail.com>
2020-09-27 14:08:24 +0200

    fixes

--- minimal/app/__init__.py
+++ minimal/app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import pylivewire
 
 app = Flask(__name__)
 pylivewire.init_pylivewire(app, "app")
+
+@app.route("/")
+def default_page():
+    return render_template("index.html")
